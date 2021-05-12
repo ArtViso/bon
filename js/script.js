b94d373a5feacaa8e70bb0aef510fec4f15e5c53
ArtViso <off.club1515@gmail.com>
2021-05-12 08:18:56 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,25 +1,9 @@
-// const navToggle = document.querySelector(".nav-toggle");
-// const mainMenu = document.querySelector(".main-menu");
-// const navToggleLabel = document.querySelector(".nav-toggle-label");
 const btnTin = document.querySelector('.btn_tin');
 const modal = document.querySelector('.modal');
 const btnTin2 = document.querySelector('.btnTin2');
 const modal2 = document.querySelector('.modal2');
 const btnTin3 = document.querySelector('.btnTin3');
 const modal3 = document.querySelector('.modal3');
-
-
-// navToggle.addEventListener("click", () => {
-//     mainMenu.classList.toggle("active");
-//     navToggleLabel.classList.toggle("active");
-// });
-
-// mainMenu.addEventListener("click", (event) => {
-//        if(event.target){
-//           mainMenu.classList.remove("active");
-//           navToggleLabel.classList.remove("active");
-//       }
-// });
 
 btnTin.onclick = function(){
     modal.style.display = "block";
